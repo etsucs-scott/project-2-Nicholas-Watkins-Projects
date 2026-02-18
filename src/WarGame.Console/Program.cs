4b70e47fe73b8d42d2e8400516af283d49397585
Nicholas-Watkins-Projects <161062138+Nicholas-Watkins-Projects@users.noreply.github.com>
2026-02-18 08:50:40 -0500

    52 card deck ready
    Added Card and Deck classes with 52 card functionality. Nothing else of note

---
+++ src/WarGame.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WarGame.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deck gameDeck = new Deck();
+            gameDeck.Shuffle();
+            gameDeck.ShowDeck();
+        }
+    }
+}
---
+++ src/WarGame.Core/CoreProgram.cs
@@ -0,0 +1,61 @@
+﻿using System.Dynamic;
+using System.Net.Security;
+
+namespace WarGame.Core;
+
+// Placeholder classes to get the idea of the structure
+public class Card
+{
+    public string suite { get; private set; }
+    public int rank { get; private set; }
+    public Dictionary<int, string> rankPairs { get; private set; } = new Dictionary<int, string>();
+    public Card(int suitePick, int rankNum)
+    {
+        string[] suites = { "Clubs", "Diamonds", "Spades", "Hearts" }; // 0, 1, 2, 3
+        if (rankNum >= 11)
+        {
+            string[] uniqueRank = { "J", "Q", "K", "A"};
+            rankPairs[rankNum] = uniqueRank[rankNum - 11];
+        }
+        else
+            rankPairs[rankNum] = $"{rankNum}";
+
+        suite = suites[suitePick];
+        rank = rankNum;
+    }
+}
+public class Deck
+{
+    private Stack<Card> deck = new Stack<Card>();
+    public Deck()
+    {
+        // 52 card deck 
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+                deck.Push(new Card(i, j + 2));
+            }
+        }
+    }
+    public void Shuffle()
+    {
+        List<Card> list = deck.ToList();
+        list = list.OrderBy(x => Random.Shared.Next()).ToList();
+        deck = new Stack<Card>(list);
+    }
+    public void ShowDeck()
+    {
+        foreach (Card card in deck)
+        {
+            if (card.suite == "Clubs" || card.suite == "Spades")
+                Console.WriteLine($"Card \"{ card.rankPairs[card.rank]}\" of {card.suite}");
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"Card \"{ card.rankPairs[card.rank]}\" of {card.suite}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
