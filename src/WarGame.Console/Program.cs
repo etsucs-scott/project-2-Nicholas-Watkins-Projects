bf40ffb0a25a08fde4f756cd9496902fc11b310e
Nicholas-Watkins-Projects <161062138+Nicholas-Watkins-Projects@users.noreply.github.com>
2026-03-06 13:55:03 -0500

    Implementing base systems
    Updated some classes but mainly started working on main systems in core

--- src/WarGame.Console/Program.cs
+++ src/WarGame.Console/Program.cs
@@ -5,15 +5,7 @@ namespace WarGame.Core
     {
         public static void Main(string[] args)
         {
-            Deck gameDeck = new Deck();
-            gameDeck.Shuffle();
-            gameDeck.ShowDeck();
 
-
-            for (int i = 0; i < 10000; i++)
-            {
-                
-            }
         }
     }
 }
--- src/WarGame.Core/CoreProgram.cs
+++ src/WarGame.Core/CoreProgram.cs
@@ -1,13 +1,14 @@
-﻿using System.Dynamic;
+﻿using System.ComponentModel.Design;
+using System.Dynamic;
 using System.Net.Security;
 
 namespace WarGame.Core;
 
-// Placeholder classes to get the idea of the structure
 public class Card
 {
     public string suite { get; private set; }
     public int rank { get; private set; }
+    public int value { get; private set; }
     public Dictionary<int, string> rankPairs { get; private set; } = new Dictionary<int, string>();
     public Card(int suitePick, int rankNum)
     {
@@ -22,11 +23,12 @@ public class Card
 
         suite = suites[suitePick];
         rank = rankNum;
+        value = rankNum;
     }
 }
 public class Deck
 {
-    public Stack<Card> cards { get; private set; } = new Stack<Card>();
+    private Stack<Card> cards = new Stack<Card>();
     public Deck()
     {
         // 52 card deck 
@@ -38,13 +40,21 @@ public class Deck
             }
         }
     }
+    public Card Draw()
+    {
+        return cards.Pop();
+    }
     public void Shuffle()
     {
         List<Card> list = cards.ToList();
         list = list.OrderBy(x => Random.Shared.Next()).ToList();
         cards = new Stack<Card>(list);
     }
-    public void ShowDeck()
+    public int GetLength()
+    {
+        return cards.Count;
+    }
+    public void ShowDeck() // Shows deck using different colors, mainly for debug and testing
     {
         foreach (Card card in cards)
         {
@@ -59,39 +69,175 @@ public class Deck
         }
     }
 }
-public class Players
+public class Hand
 {
-    // public Queue<Card> hand { get; private set; } // Should check if this is accepabled for hand rule
-    public Dictionary<string, Queue<Card>> playerHands { get; private set; } = new Dictionary<string, Queue<Card>>();
-    public Players(int playerCount) // Makes all player hands | Should add 4 player restriction #### NEEDED ####
+    private Queue<Card> hand = new Queue<Card>();
+    public Card GetCard()
     {
-        for (int i = 0; i < playerCount; i++)
-        {
-            playerHands[$"Player {i + 1}"] =  new Queue<Card>();
-        }
+        return hand.Dequeue();
     }
-    public void Deal(Deck deck) // Should deal cards in round robin order
+    public void AddCard(Card card)
     {
-        deck.Shuffle();
-        int i = 0;
-        foreach (Card card in deck.cards)
+        hand.Enqueue(card);
+    }
+    public int GetCardCount()
+    {
+        return hand.Count;
+    }
+}
+public class PlayerHand
+{
+    private Dictionary<String, Hand> playerhands = new Dictionary<String, Hand>();
+    public Hand GetHand(String player)
+    {
+        return playerhands[player];
+    }
+    public void UpdateHand(String player, Hand hand)
+    {
+        playerhands[player] = hand;
+    }
+    public List<String> GetPlayers()
+    {
+        return playerhands.Keys.ToList();
+    }
+    public void RemovePlayer(String player)
+    {
+        playerhands.Remove(player);
+    }
+    public void AddPlayer(String player, Hand hand)
+    {
+        playerhands[player] = hand;
+    }
+}
+public class PlayedCards
+{
+    private Dictionary<String, Card> playedCards = new Dictionary<String, Card>();
+    public Card GetCard(String player)
+    {
+        return playedCards[player];
+    }
+    public List<Card> GetCards()
+    {
+        return playedCards.Values.ToList();
+    }
+    public void UpdateCard(String player, Card card)
+    {
+        playedCards[player] = card;
+    }
+    public (List<String>, int) GetHighest()
+    {
+        int highestNum = 0;
+        List<String> highPlayers = new List<String>();
+
+        List<String> players = playedCards.Keys.ToList();
+        List<Card> cards = playedCards.Values.ToList();
+
+        // Find the highest ranked players and track them for tie handling
+        for (int i = 0; i < playedCards.Count(); i++)
         {
-            if (i >= playerHands.Keys.Count())
-                i = 0;
+            if (cards[i].value > highestNum)
+            {
+                highestNum = cards[i].value;
+                highPlayers = [players[i]];
+            }
+            else if (cards[i].value == highestNum)
+                highPlayers.Add(players[i]);
+        }
+        // Remove tied players/winner
+        foreach (String player in highPlayers)
+        {
+            playedCards.Remove(player);
+        }
 
-            Queue<Card> hand = playerHands[$"Player {i + 1}"];
-            hand.Enqueue(card);
+        return (highPlayers, highestNum);
+    }
+}
+public class Pot
+{
+    private List<Card> pot = new List<Card>();
+    public void AddCard(Card card)
+    {
+        pot.Add(card);
+    }
+    public void SetPot(List<Card> pot)
+    {
+        this.pot = pot;
+    }
+}
+public class TiePot
+{
+    private List<Card> tiePot = new List<Card>();
+    public void AddCard(Card card)
+    {
+        tiePot.Add(card);
+    }
+    public void SetPot(List<Card> tiePot)
+    {
+        this.tiePot = tiePot;
+    }
+}
+public class Deal
+{
+    public void DealCards(PlayerHand playerHand, Deck deck) // Deals card in round robin order
+    {
+        int playerChoice = 0;
+        List<String> players = playerHand.GetPlayers();
+        while (deck.GetLength() > 0)
+        {
+            if (playerChoice > 4)
+                playerChoice = 0;
 
-            i++; 
+            String player = players[playerChoice];
+
+            Card card = deck.Draw();
+            Hand hand = playerHand.GetHand(player);
+            hand.AddCard(card);
+            playerHand.UpdateHand(player, hand); 
         }
     }
 }
-public class Game
+public class Round
 {
-    public List<Card> pot { get; private set; }
-    public List<Card> tiePot { get; private set; }
-    public void Round(Players players)
+    public void RoundLoop(Pot pot, PlayerHand playerHand)
     {
-        Console.WriteLine("TESTING...");
+        List<String> players = playerHand.GetPlayers();
+
+        // Handle players without a card
+        foreach (String player in players) // Check if player doesnt has card and removes them from PlayerHand if they dont
+        {
+            if (playerHand.GetHand(player).GetCardCount() < 0)
+            {
+                playerHand.RemovePlayer(player);
+                Console.WriteLine($"{player} has lost!");
+            }
+        }
+
+        // Player hand cards to played cards 
+        PlayedCards playedCards = new PlayedCards();
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Card card = playerHand.GetHand(players[0]).GetCard();
+            playedCards.UpdateCard(players[0], card); // Add played card from each player from hand
+        }
+
+        // Add cards to pot
+        (List<String>, int) tieChecker = playedCards.GetHighest();
+        foreach (Card card in playedCards.GetCards())
+        {
+            pot.AddCard(card);
+        }
+
+        // Tie handling
+        if (tieChecker.Item1.Count() != 0)
+        {
+            Console.WriteLine("HIT A TIE. Waiting...");
+            Console.ReadLine();
+        }
+
+
     }
+}
+public class EndGameChecking
+{
+
 }
